08b2181726456070ff1ab17bbee8b7adc9587e7f
i22l <123123259+i22l@users.noreply.github.com>
2024-08-02 06:18:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ app = Client(
     "autoPost",
     api_id="29510141",
     api_hash="14c074a5aed49dc7752a9f8d54cf4ad4",
-    bot_token="6357128136:AAGOGrC9uvfaZX_Hpy24DRWd4gbLpNztZqI",#توكنك
+    bot_token="6357128136:AAEjHDWbZDsFmHXYIHH8FBwn596dfc6zS0g",#توكنك
 )
 loop = get_event_loop()
 listener = Listener(client = app)
